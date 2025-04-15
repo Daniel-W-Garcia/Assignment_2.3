9cfbb063a70c7fff068532464f5a31f0a6dd1929
itodobien <daniel.w.garcia@outlook.com>
2025-04-15 08:31:15 -0700

    First attempt at tip calculator
    simple calculator and user interface. Bill amount is hard coded for now

---
+++ Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment_2._3;
+
+public class Calculator
+{
+    
+    public float TipAmount { get ; set; }
+    public float BillAmount { get; set; }
+    public float TotalAmount { get; set; }
+
+    public float CalculateTipAmount(float billAmount, float tipPercentage)
+    {
+        TipAmount = billAmount * tipPercentage / 100;
+        return TipAmount;
+    }
+    public float CalculateTotalAmount(float billAmount, float tipAmount)
+    {
+        TotalAmount = billAmount + tipAmount;
+        return TotalAmount;
+    }
+
+    public void GetTipPercentageAndTotalBill()
+    {
+        Console.WriteLine("Enter the tip percentage: ");
+        float tipPercentage = float.Parse(Console.ReadLine());
+        CalculateTipAmount(BillAmount, tipPercentage);
+        CalculateTotalAmount(BillAmount, TipAmount);
+        PrintTransaction();
+    }
+    
+    public void PrintTransaction()
+    {
+        Console.WriteLine($"""
+                           Your bill is {BillAmount}
+                           Your tip is {TipAmount}
+                           Your total is {TotalAmount}
+                           """);
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Assignment_2._3;
+
+Calculator calculator = new()
+{
+    BillAmount = 100
+};
+
+calculator.GetTipPercentageAndTotalBill();
